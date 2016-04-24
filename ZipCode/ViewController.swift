7d9920a66906ea3c030d073643639e23e2f353cf
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2016-04-24 10:15:11 +0900

    initial commit

---
+++ ZipCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZipCode
+//
+//  Created by Hoshi Takanori on 2016/04/24.
+//  Copyright © 2016年 Hoshi Takanori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
