47c70d2995b6986227d73289c4513c7462ec8915
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2016-04-27 18:46:28 +0900

    add start button

--- ZipCode/ViewController.swift
+++ ZipCode/ViewController.swift
@@ -8,26 +8,31 @@
 
 import UIKit
 import APIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var textView: UITextView!
+
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Run", style: .Plain, target: self, action: #selector(run))
+        startButton.addTarget(self, action: #selector(start), forControlEvents: .TouchUpInside)
     }
 
-    func run() {
+    func start() {
         let request = ZipCodeRequest(zipCode: "100-0001")
         Session.sendRequest(request) { result in
             switch result {
             case .Success(let result):
-                print("Success: \(result)")
+                self.textView.text = "Success: \(result)"
             case .Failure(let error):
-                print("Failure: \(error)")
+                self.textView.text = "Failure: \(error)"
             }
         }
     }
